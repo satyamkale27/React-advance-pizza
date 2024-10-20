81d48f1f61332f6225744a6933f546fdda468f4b
satyam <satyamkale271@gmail.com>
2024-10-20 19:51:45 +0530

    cart item quantity shown in menuItem to increase or decrease quantity

--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -3,6 +3,7 @@ import { formatCurrency } from "../utlities/helpers";
 import Button from "../../ui/Button";
 import { addItem, getCurrentQuantityById } from "../cart/cartSlice";
 import DeleteItem from "../cart/DeleteItem";
+import UpdateItemQuantity from "../cart/UpdateItemQuantty";
 
 // reducre from cartSlice beacuse some functionality of cart are needed in menu like
 function MenuItem({ pizza }) {
@@ -45,7 +46,15 @@ function MenuItem({ pizza }) {
             </p>
           )}
 
-          {isIncart && <DeleteItem pizzaId={id} />}
+          {isIncart && (
+            <div className="flex items-center gap-3 sm:gap-8">
+              <UpdateItemQuantity
+                pizzaId={id}
+                currentQuantity={currentQuantity}
+              />
+              <DeleteItem pizzaId={id} />
+            </div>
+          )}
 
           {!soldOut && !isIncart && (
             <Button type="small" onClick={handelAddToCart}>
